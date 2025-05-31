ff3eae2dce25bab7e93db07c81856d87fb224765
fenglin <1349410043@qq.com>
2025-05-31 22:02:47 +0800

    modify model name

--- few_shot/run.py
+++ few_shot/run.py
@@ -627,7 +627,7 @@ if __name__ == "__main__" :
     # )
 
     model = ApiLLMModel(
-        model_name="qwen3-8b_think",
+        model_name="qwen3-8b-think",
         api_base="http://127.0.0.1:5001/v2/",
         api_key='23333333',
         system_prompt=TRAIN_PROMPT_ZERO_SHOT_V2,
