3d572de8dabb6642908245d236d38e5633d569f1
fenglin <1349410043@qq.com>
2025-06-04 19:43:22 +0800

    upload train code

--- finetune/exmple.py
+++ finetune/exmple.py
@@ -133,8 +133,8 @@ def run():
     # model_dir = snapshot_download("Qwen/Qwen3-8B", cache_dir="models/", revision="master")
 
     # Transformers加载模型权重
-    tokenizer = AutoTokenizer.from_pretrained("models/Qwen/Qwen3-8B", use_fast=False, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained("models/Qwen/Qwen3-8B", device_map="auto", torch_dtype=torch.bfloat16)
+    tokenizer = AutoTokenizer.from_pretrained("models/Qwen3-8B", use_fast=False, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained("models/Qwen3-8B", device_map="auto", torch_dtype=torch.bfloat16)
     model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
 
     def process_func(example):
@@ -233,11 +233,11 @@ def run():
 
 def run_lora():
     # 在modelscope上下载Qwen模型到本地目录下
-    model_dir = snapshot_download("Qwen/Qwen3-1.7B", cache_dir="models/", revision="master")
+    model_dir = snapshot_download("models/Qwen3-8B", cache_dir="models/", revision="master")
 
     # Transformers加载模型权重
-    tokenizer = AutoTokenizer.from_pretrained("models/Qwen/Qwen3-1.7B", use_fast=False, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained("models/Qwen/Qwen3-1.7B", device_map="auto", torch_dtype=torch.bfloat16)
+    tokenizer = AutoTokenizer.from_pretrained("models/Qwen3-8B", use_fast=False, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained("models/Qwen3-8B", device_map="auto", torch_dtype=torch.bfloat16)
     model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
 
     def process_func(example):
