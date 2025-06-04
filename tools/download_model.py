1abf3b60b0153e36715a1267c9451ed6f588c2bf
fenglin <1349410043@qq.com>
2025-06-04 16:04:32 +0800

    upload fintune code

--- tools/download_model.py
+++ tools/download_model.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModel, Qwen2TokenizerFast
 from transformers import AutoTokenizer, AutoModel, Qwen2TokenizerFast
 
 
-tokenizer: Qwen2TokenizerFast = AutoTokenizer.from_pretrained("models/Qwen3-8B")
+
 tokenizer: Qwen2TokenizerFast = AutoTokenizer.from_pretrained("models/Qwen3-8B")
 print(tokenizer)
 messages = [{'content': '你好，你是谁？', 'role': 'system'}, {'content': '你好，你是谁？', 'role': 'user'}]
@@ -14,7 +14,7 @@ text = tokenizer.apply_chat_template(
 )
 inputs = tokenizer(text).input_ids
 
-print(text)
+print([text])
 print(inputs)
 # model = AutoModel.from_pretrained("models/Qwen3-8B")
 
