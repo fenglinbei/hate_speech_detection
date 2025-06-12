363b4d40d0911cbedc8b3fa44c96030bb61ad5e0
fenglin <1349410043@qq.com>
2025-06-12 10:49:09 +0800

    update train code

--- few_shot/run.py
+++ few_shot/run.py
@@ -515,11 +515,15 @@ def create_model_from_config(model_config: dict) -> Any:
     params: dict = model_config.get('params', {})
 
     if model_type == "LLM":
-        return LLM(model_path=params.get("model_path"))
+        return LLM(
+            model_path=params.get("model_path"),
+            model_name=params.get("model_name")
+            )
     elif model_type == 'VLLM':
         # 从参数中提取VLLM配置
         return VLLM(
             model_path=params.get("model_path"),
+            model_name=params.get("model_name")
             tensor_parallel_size=params.get("tensor_parallel_size", 1),
             max_num_seqs=params.get("max_num_seqs", 32),
             max_model_len=params.get("max_model_len", 8192 * 3 // 2),
