95e166ce1b75ba51b27064ad28172880c86e39d1
fenglin <1349410043@qq.com>
2025-06-24 11:42:25 +0800

    lex train

--- finetune/make_data.py
+++ finetune/make_data.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 from typing import Optional
 
 from prompt import *
-from rag.core import Retriever
+from rag.core import Retriever, LexiconRetriever
 from tools.convert import output2triple
 
 def dataset_transfer_no_think_test(raw_data_path: str, test_output_path: str, prompt_template: str, system_prompt: Optional[str] = None):
@@ -80,5 +80,106 @@ def dataset_transfer_no_think(
         for message in messages[split_idx:]:
             file.write(json.dumps(message, ensure_ascii=False) + "\n")
 
+def make_lexcion_rag_data(
+        raw_data_path: str, 
+        train_output_path: str, 
+        val_output_path: str, 
+        prompt_template: str, 
+        example_template: str,
+        system_prompt: Optional[str] = None,
+        srag_top_k: int = 1,
+        lex_top_k: int = -1
+        ):
+    """转换训练/验证集数据格式"""
+
+    messages = []
+    with open(raw_data_path, "r") as file:
+        raw_datas = json.load(file)
+
+    split_idx = int(len(raw_datas) * 0.9)
+    srag_retriever = Retriever(model_path="./models/bge-large-zh-v1.5", model_name="bge-large-zh-v1.5")
+    lex_retriever = LexiconRetriever(model_path="./models/bge-large-zh-v1.5", model_name="bge-large-zh-v1.5", data_path="data/lexicon/annotated_lexicon.json")
+    
+    srag_retriever.create_embeddings(raw_datas[:split_idx])
+    
+    pbar = tqdm(
+            total=len(raw_datas),
+            desc=f"Preprocessing datas",
+            unit="item",
+            dynamic_ncols=True,
+            leave=True
+        )
+
+    for raw_data in raw_datas[:split_idx]:
+        triples = []
+        for quadruple in raw_data["quadruples"]:
+            label = quadruple["targeted_group"]
+            triples.append(f"{quadruple['target']} | {quadruple['argument']} | {label}")
+        
+
+        retrieve_contents, retrieve_outputs = srag_retriever.retrieve(raw_data['content'], srag_top_k)
+        examples = []
+        for retrieve_content, retrieve_output in zip(retrieve_contents, retrieve_outputs):
+            example_prompt = example_template.replace("{retrieve_content}", retrieve_content).\
+                                              replace("{retrieve_output}", output2triple(retrieve_output))
+            examples.append(example_prompt)
+
+        lex_contents = lex_retriever.including_retrieve(raw_data['content'], lex_top_k)
+            
+        input = prompt_template.replace("{examples}", "\n".join(examples)).\
+                                replace("{lexicons}", "\n".join(lex_contents)).\
+                                replace("{text}", raw_data["content"])
+
+        answer = " [SEP] ".join(triples) + " [END]"
+        message = {"instruction": system_prompt if system_prompt else "", "input": f"{input}", "output": answer, "content": raw_data["content"]}
+        messages.append(message)
+        pbar.update(1)
+    
+    with open(train_output_path, "w", encoding="utf-8") as file:
+        for message in messages:
+            file.write(json.dumps(message, ensure_ascii=False) + "\n")
+    
+    messages = []
+    srag_retriever.create_embeddings(raw_datas)
+
+    for raw_data in raw_datas[split_idx:]:
+        triples = []
+        for quadruple in raw_data["quadruples"]:
+            label = quadruple["targeted_group"]
+            triples.append(f"{quadruple['target']} | {quadruple['argument']} | {label}")
+        
+
+        retrieve_contents, retrieve_outputs = srag_retriever.retrieve(raw_data['content'], srag_top_k)
+        examples = []
+        for retrieve_content, retrieve_output in zip(retrieve_contents, retrieve_outputs):
+            example_prompt = example_template.replace("{retrieve_content}", retrieve_content).\
+                                              replace("{retrieve_output}", output2triple(retrieve_output))
+            examples.append(example_prompt)
+
+        lex_contents = lex_retriever.including_retrieve(raw_data['content'], lex_top_k)
+            
+        input = prompt_template.replace("{examples}", "\n".join(examples)).\
+                                replace("{lexicons}", "\n".join(lex_contents)).\
+                                replace("{text}", raw_data["content"])
+
+        answer = " [SEP] ".join(triples) + " [END]"
+        message = {"instruction": system_prompt if system_prompt else "", "input": f"{input}", "output": answer, "content": raw_data["content"]}
+        messages.append(message)
+        pbar.update(1)
+
+    
+    with open(val_output_path, "w", encoding="utf-8") as file:
+        for message in messages[split_idx:]:
+            file.write(json.dumps(message, ensure_ascii=False) + "\n")
+
 if __name__ == "__main__":
-    dataset_transfer_no_think("data/full/std/train.json", "finetune/data/train_full.jsonl", "finetune/data/val.jsonl", RAG_PROMPT_USER_V1, system_prompt=QWEN2_DEFAULT_SYSTEM_PROMPT)
+    # dataset_transfer_no_think("data/full/std/train.json", "finetune/data/train_full.jsonl", "finetune/data/val.jsonl", RAG_PROMPT_USER_V1, system_prompt=QWEN2_DEFAULT_SYSTEM_PROMPT)
+    make_lexcion_rag_data(
+        raw_data_path="data/full/std/train.json", 
+        train_output_path="finetune/data/train_lex_rag.jsonl", 
+        val_output_path="finetune/data/val_lex_rag.jsonl",
+        prompt_template=RAG_PROMPT_USER_V2,
+        example_template=RAG_PROMPT_EXAMPLE_V2,
+        system_prompt=QWEN2_DEFAULT_SYSTEM_PROMPT,
+        srag_top_k=1,
+        lex_top_k=-1)
--- rag/core.py
+++ rag/core.py
@@ -7,6 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 from collections import Counter
 
+from prompt import *
 from tools.convert import output2triple, parsed_quad_to_raw_quad
 
 class Retriever:
@@ -82,3 +83,90 @@ class Retriever:
                 break
 
         return unique_texts, unique_outputs
+    
+class LexiconRetriever:
+
+    def __init__(self, model_path: str, model_name: str, data_path: str | None = None, device: str = "cuda:0"):
+        logger.info(f"Loading model from path: {model_path}")
+        self.model = SentenceTransformer(model_path).to(device)
+        self.model_name = model_name
+
+        if data_path:
+            self.load_datas(data_path)
+            self.create_embeddings()
+
+    def create_embeddings(self, datas: Optional[list[dict]] = None):
+        logger.info("Processing embedding")
+        if not datas:
+            texts = self.texts
+        else:
+            self.texts = [item['content'] for item in datas]
+            self.test2item = {}
+            for item in datas:
+                item['output'] = parsed_quad_to_raw_quad(item['quadruples'])
+                self.test2item[item['content']] = item
+            texts = self.texts
+
+        corpus_embeddings = self.model.encode(texts, convert_to_tensor=True, show_progress_bar=True)
+        if corpus_embeddings.is_cuda:
+            corpus_embeddings = corpus_embeddings.cpu()
+        self.corpus_embeddings_np = corpus_embeddings.numpy()
+
+    def load_datas(self, data_path: str):
+        datas = load_json(data_path)["terms"]
+        self.texts = []
+        self.word2item = {}
+        for data in datas:
+            prompt = LEXICON_RAG_PROMPT.replace("{word}", data["term"]).\
+                                        replace("{category}", data["category"]).\
+                                        replace("{definition}", data["definition"])
+            self.texts.append(prompt)
+            self.word2item[data["term"]] = prompt
+        
+    
+    def similarity_retrieve(self, query: str, top_k: int = 1, deduplicate: bool = True) -> list[str]:
+        query_embedding = self.model.encode(query, convert_to_tensor=True, show_progress_bar=False)
+
+        if query_embedding.is_cuda:
+            query_embedding = query_embedding.cpu()
+
+        query_embedding_np = query_embedding.numpy().reshape(1, -1)
+
+        similarities = cosine_similarity(query_embedding_np, self.corpus_embeddings_np)[0]
+
+        # 获取所有索引并按相似度排序
+        sorted_indices = np.argsort(similarities)[::-1]
+
+        unique_texts = []
+        seen_contents = set() if not deduplicate else set([query])  # 用于追踪已处理的内容
+        
+        # 遍历所有排序后的索引
+        for idx in sorted_indices:
+            content = self.texts[idx]
+            
+            # 去重逻辑
+            if deduplicate:
+                if content in seen_contents:
+                    continue  # 已处理过相同内容，跳过
+                seen_contents.add(content)
+            
+            unique_texts.append(content)
+            
+            # 达到需要的 top_k 数量时停止
+            if len(unique_texts) >= top_k:
+                break
+
+        return unique_texts
+    
+    def including_retrieve(self, query: str, top_k: int = -1, deduplicate: bool = True) -> list[str]:
+        result = []
+        for word in self.word2item.keys():
+            if word in query:
+                result.append(self.word2item[word])
+        
+        return result if top_k == -1 else result[:top_k]
+
+
+if __name__ == "__main__":
+    retriever = LexiconRetriever(model_path="./models/bge-large-zh-v1.5", model_name="bge-large-zh-v1.5", data_path="data/lexicon/annotated_lexicon.json")
+    print(retriever.including_retrieve("那些嫁给默的国女能自愿放弃中国国籍，绝对值得立牌坊。", top_k=-1))
