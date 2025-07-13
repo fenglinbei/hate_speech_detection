e5a3c5039901aa9cca845b60f66c06b303e02152
fenglin <1349410043@qq.com>
2025-07-13 16:10:10 +0800

    new train data

---
+++ finetune/check_data.py
@@ -0,0 +1,24 @@
+import json
+
+with open('finetune/data/simlex5_rag5/train.jsonl', 'r') as f:
+    lines = f.readlines()
+datas = [json.loads(line) for line in lines]
+
+max_length = 2000
+over_idx = []
+for data in datas:
+    if len(data['input']) > max_length:
+        length = len(data['input'])
+        idx = data['id']
+        print(f'length: {length}, index: {idx}')
+        over_idx.append(idx)
+
+print(f'over length: {len(over_idx)}')
+with open('finetune/data/simlex5_rag5/new_train.jsonl', 'w') as f:
+    for data in datas:
+        if data['id'] not in over_idx:
+            f.write(json.dumps(data, ensure_ascii=False) + '\n')
+print('done')
+print(f'new data length: {len(datas) - len(over_idx)}')
+print(f'new data path: finetune/data/simlex5_rag5/new_train.json')
+print('check data done')
