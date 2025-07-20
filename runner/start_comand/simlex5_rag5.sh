361260c4eac838a0cb4167c09afdf5cb48d3c5a8
fenglinbei <1349410043@qq.com>
2025-07-20 10:01:25 +0800

    result update

--- runner/start_comand/simlex5_rag5.sh
+++ runner/start_comand/simlex5_rag5.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
 --served-model-name checkpoint-724 \
---model=./models/qwen2.5-7B-instruct-simlex5-rag5-trip-noalp \
+--model=./models/qwen2.5-7B-instruct-simlex5-rag5-trip-noalp/checkpoint-720 \
 --trust-remote-code \
 --tensor-parallel-size=1 \
 --port="35003" \
