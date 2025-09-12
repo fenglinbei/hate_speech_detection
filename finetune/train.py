3987cc86b80b0c9ff3d1b707d10803a635587b97
fenglinbei <1349410043@qq.com>
2025-09-12 21:42:26 +0800

    new_data

--- finetune/train.py
+++ finetune/train.py
@@ -283,8 +283,7 @@ def run(config: dict):
         callbacks=[SwanLabCallback(
             project=os.environ["SWANLAB_PROJECT"],
             experiment_name=config['exp_name'],
-        )],
-        save_model_only=True
+        )]
     )
 
     # if not isinstance(model, DeepSpeedEngine):
