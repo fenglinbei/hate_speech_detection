1bf0cc3be990f3a2db8f24f04f1bf71e0aa3cb5a
fenglin <1349410043@qq.com>
2025-10-28 21:33:56 +0800

    new data

--- prompt.py
+++ prompt.py
@@ -144,6 +144,14 @@ RAG_PROMPT_USER_V2 = """你是一个内容审查专家，请你分析我的句�
 ### 三元组：
 """
 
+RAG_PROMPT_USER_V5 = """你是一个内容审查专家，请你分析我的句子并且从中提取出一个或者多个三元组:
+示例：
+{examples}
+### 句子：
+{text}
+### 三元组：
+"""
+
 RAG_PROMPT_EXAMPLE_V2 = """### 句子：
 {retrieve_content}
 ### 三元组：
