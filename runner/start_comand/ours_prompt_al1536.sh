166646b3745879104d2323d10fb6f522efbbe371
fenglin <1349410043@qq.com>
2025-10-30 10:33:41 +0800

    new data

--- runner/start_comand/ours_prompt_al1536.sh
+++ runner/start_comand/ours_prompt_al1536.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
 --served-model-name checkpoint-1446 \
---model=./models/qwen2.5-7B-instruct-ours-prompt-al2048/checkpoint-1446 \
+--model=./models/qwen2.5-7B-instruct-ours-prompt-al1536/checkpoint-1446 \
 --trust-remote-code \
 --tensor-parallel-size=1 \
 --port="35003" \
