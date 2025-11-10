da0521e37e3d5559100dceccef1f66acbb13f75e
fenglin <1349410043@qq.com>
2025-11-10 11:53:43 +0800

    chatglm3

--- prompt.py
+++ prompt.py
@@ -1,4 +1,5 @@
 QWEN2_DEFAULT_SYSTEM_PROMPT = "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."
+CHATGLM3_DEFAULT_SYSTEM_PROMPT = "You are ChatGLM3, a large language model trained by Zhipu.AI. Follow the user's instructions carefully."
 
 TRAIN_PROMPT_FEW_SHOT_V1 = """请分析以下文本，识别其中的评论对象、论点、是否仇恨和仇恨类别。
 
