fb1838dc8ea35a344fcd02cebbec635c349c51c7
fenglin <1349410043@qq.com>
2025-11-15 02:43:39 +0800

    before

--- runner/start_comand/before/Qwen3-8B.sh
+++ runner/start_comand/before/Qwen3-8B.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
 --served-model-name qwen3 \
---model=./models/exps/before/Qwen3-8B/checkpoint-1444 \
+--model=./models/exps/before/Qwen3-8B/checkpoint-1446 \
 --trust-remote-code \
 --tensor-parallel-size=1 \
 --port="35003" \
