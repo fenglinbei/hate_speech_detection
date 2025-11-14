1319ae5fe369863de9280c4031a965ac5f5e0839
fenglin <1349410043@qq.com>
2025-11-15 02:43:39 +0800

    before

--- runner/start_comand/before/GLM-4-9B-Chat.sh
+++ runner/start_comand/before/GLM-4-9B-Chat.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
 --served-model-name glm4 \
---model=./models/exps/before/GLM-4-9B-Chat/checkpoint-1444 \
+--model=./models/exps/before/GLM-4-9B-Chat/checkpoint-1446 \
 --trust-remote-code \
 --tensor-parallel-size=1 \
 --port="35003" \
