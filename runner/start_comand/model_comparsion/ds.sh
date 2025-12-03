5760412ad161c5fc2837da85f2885f9945bcb0f3
fenglinbei <1349410043@qq.com>
2025-12-03 11:35:37 +0800

    ds after test done

--- runner/start_comand/model_comparsion/ds.sh
+++ runner/start_comand/model_comparsion/ds.sh
@@ -4,4 +4,4 @@ CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
 --trust-remote-code \
 --tensor-parallel-size=1 \
 --port="35003" \
---max_model_len 8192
+--max_model_len 4096
