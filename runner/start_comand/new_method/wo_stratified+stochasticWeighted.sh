122e90e6dc1d44fac7dc97a13407cde34488256d
fenglin <1349410043@qq.com>
2025-12-09 13:13:02 +0800

    new method

--- runner/start_comand/new_method/wo_stratified+stochasticWeighted.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
---served-model-name qwen2.5 \
---model=./models/exps/new_method/wo_stratified+stochasticWeighted/checkpoint-1444 \
---trust-remote-code \
---tensor-parallel-size=1 \
---port="35003" \
---max_model_len 8192
