22bdd3ec6137fd4f8f5e0408d5d2733de57d1cc4
fenglin <1349410043@qq.com>
2025-12-17 00:33:54 +0800

    new exp

---
+++ runner/start_comand/new_method/clustered_n4.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
+--served-model-name qwen2.5 \
+--model=./models/exps/new_method/clustered_n4/checkpoint-1444 \
+--trust-remote-code \
+--tensor-parallel-size=1 \
+--port="35003" \
+--max_model_len 8192
