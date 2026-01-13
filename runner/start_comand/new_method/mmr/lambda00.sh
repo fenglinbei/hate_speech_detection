31db114be8cd81b8d8b243e47b3735153f0a425f
fenglin <1349410043@qq.com>
2026-01-13 14:54:01 +0800

    mmr

---
+++ runner/start_comand/new_method/mmr/lambda00.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
+--served-model-name qwen2.5 \
+--model=./models/exps/mmr/lambda00/checkpoint-1446 \
+--trust-remote-code \
+--tensor-parallel-size=1 \
+--port="35003" \
+--max_model_len 8192
