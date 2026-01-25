0148bee2e576721773cfe41a3163e954fdae8c32
fenglinbei <1349410043@qq.com>
2026-01-26 00:57:11 +0800

    new data

--- finetune/train.sh
+++ finetune/train.sh
@@ -1,1 +1,1 @@
-torchrun --nproc_per_node=4 finetune/train.py --config finetune/config/new_method/unform_stratified.json
+CUDA_VISIBLE_DEVICES=0,2,3 torchrun --nproc_per_node=3 finetune/train.py --config finetune/config/new_method/unform_stratified.json
