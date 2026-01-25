aeeb7df23dc12a379264c026d98e118233313413
fenglinbei <1349410043@qq.com>
2026-01-25 23:35:32 +0800

    new result

--- runner/start_comand/new_method/mmr/lambda00.sh
+++ runner/start_comand/new_method/mmr/lambda00.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
+CUDA_VISIBLE_DEVICES="2,3" python -m vllm.entrypoints.openai.api_server \
 --served-model-name qwen2.5 \
 --model=./models/exps/mmr/lambda00/checkpoint-1446 \
 --trust-remote-code \
---tensor-parallel-size=1 \
+--tensor-parallel-size=2 \
 --port="35003" \
 --max_model_len 8192
