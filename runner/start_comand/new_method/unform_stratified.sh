72af7b2c6659cc43c9f531342ed96608c2381a16
fenglin <1349410043@qq.com>
2026-01-26 16:46:23 +0800

    train arg change

--- runner/start_comand/new_method/unform_stratified.sh
+++ runner/start_comand/new_method/unform_stratified.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES="1" python -m vllm.entrypoints.openai.api_server \
+CUDA_VISIBLE_DEVICES="0,1,2,3" python -m vllm.entrypoints.openai.api_server \
 --served-model-name qwen2.5 \
 --model=./models/exps/new_method/unform_stratified/checkpoint-1444 \
 --trust-remote-code \
---tensor-parallel-size=1 \
+--tensor-parallel-size=4 \
 --port="35003" \
 --max_model_len 8192
