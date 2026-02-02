c4dd2dfa98574c23bb582c35e18ccdde59814bf2
fenglin <1349410043@qq.com>
2026-02-02 17:18:31 +0800

    exp auto

--- tools/build_graph.sh
+++ tools/build_graph.sh
@@ -1,1 +1,1 @@
-python tools/build_graph.py runner/output/new_method/mmr --pattern "*.json" --out1 data/graphs/mmr_graph_1.csv --out2 data/graphs/mmr_graph_2.csv --out2_flat data/graphs/mmr_graph_2_flat.csv
+python tools/build_graph.py runner/output/model_comparison/after --pattern "*.json" --out1 analyse/model/model_after_graph_1.csv --out2 analyse/model/model_after_graph_2.csv --out2_flat analyse/model/model_after_graph_2_flat.csv
