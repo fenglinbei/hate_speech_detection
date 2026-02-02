eda5f0eaae181eb2122a49be3af587acadbf2a11
fenglin <1349410043@qq.com>
2026-02-02 19:54:13 +0800

    new ablation exp

---
+++ scripts/build_graph.sh
@@ -0,0 +1,5 @@
+python scripts/build_graph.py runner/output/model_comparison/after \
+ --pattern "*.json" \
+ --out1 analyse/model/model_after_graph_1.csv \
+ --out2 analyse/model/model_after_graph_2.csv \
+ --out2_flat analyse/model/model_after_graph_2_flat.csv
--- tools/build_graph.sh
+++
@@ -1,1 +0,0 @@
-python tools/build_graph.py runner/output/model_comparison/after --pattern "*.json" --out1 analyse/model/model_after_graph_1.csv --out2 analyse/model/model_after_graph_2.csv --out2_flat analyse/model/model_after_graph_2_flat.csv
