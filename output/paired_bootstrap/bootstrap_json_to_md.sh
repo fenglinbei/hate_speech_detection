b11b934497b8614938e7f92a366d42981469af8c
fenglin <1349410043@qq.com>
2026-04-27 01:40:53 +0800

    add save model

---
+++ output/paired_bootstrap/bootstrap_json_to_md.sh
@@ -0,0 +1,4 @@
+python output/paired_bootstrap/bootstrap_json_to_md.py output/paired_bootstrap/uniform_vs_main.json output/paired_bootstrap/wo_semantic_vs_main.json \
+  --names "Ours(Uniform)" "w/o semantic" \
+  --note \
+  > bootstrap_table.md
