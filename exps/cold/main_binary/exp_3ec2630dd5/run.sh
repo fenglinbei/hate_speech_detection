717448ca41499aaeb5113d0064be5ed7b4052a94
root <1349410043@qq.com>
2026-04-27 17:32:33 +0800

    COLD Binary Result

--- exps/cold/main_binary/exp_3ec2630dd5/run.sh
+++ exps/cold/main_binary/exp_3ec2630dd5/run.sh
@@ -1,5 +1,6 @@
 MODE=full \
+TRAIN_LORA=1 \
 TRAIN_BACKEND=deepspeed \
-TRAIN_PROFILE=ds_zero3_safe \
+TRAIN_PROFILE=ds_zero2_safe \
 TRAIN_CUDA_VISIBLE_DEVICES=0,1,2,3 \
 bash scripts/exps/run_one_exp.sh exps/cold/main_binary/exp_3ec2630dd5/
