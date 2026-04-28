5be91ba5477e427eec8c7ecbdaa3df9177337ffd
fenglinbei <1349410043@qq.com>
2026-04-28 13:27:14 +0800

    COLD Zero-Shot

--- exps/cold/baselines/exp_zero_shot_d55cfa6534/run.sh
+++ exps/cold/baselines/exp_zero_shot_d55cfa6534/run.sh
@@ -1,6 +1,6 @@
-MODE=data\
+MODE=full \
 TRAIN_BACKEND=deepspeed \
-TRAIN_PROFILE=ds_zero2_safe \
+TRAIN_PROFILE=ds_zero2_bs4 \
 TRAIN_CUDA_VISIBLE_DEVICES=0,1,2,3 \
 TRAIN_LORA=1 \
 bash scripts/exps/run_one_exp.sh exps/cold/baselines/exp_zero_shot_d55cfa6534
