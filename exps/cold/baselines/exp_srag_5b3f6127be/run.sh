39580d109c7d70f5805f76d0bd7bf93230b67869
fenglinbei <1349410043@qq.com>
2026-04-28 14:40:51 +0800

    debug

--- exps/cold/baselines/exp_srag_5b3f6127be/run.sh
+++ exps/cold/baselines/exp_srag_5b3f6127be/run.sh
@@ -1,7 +1,7 @@
 MODE=full \
 TRAIN_BACKEND=deepspeed \
 TRAIN_PROFILE=ds_zero2_bs4 \
+TRAIN_MAX_STEPS=4 \
 TRAIN_CUDA_VISIBLE_DEVICES=0,1,2,3 \
 TRAIN_LORA=1 \
-TRAIN_DS_AUTOTUNE=1 \
 bash scripts/exps/run_one_exp.sh exps/cold/baselines/exp_srag_5b3f6127be
