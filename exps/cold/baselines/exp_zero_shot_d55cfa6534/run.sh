90e4d435192888552e4a18cf7a07e61a9ab638b9
fenglinbei <1349410043@qq.com>
2026-04-28 12:30:23 +0800

    build exp dir

---
+++ exps/cold/baselines/exp_zero_shot_d55cfa6534/run.sh
@@ -0,0 +1,6 @@
+MODE=data\
+TRAIN_BACKEND=deepspeed \
+TRAIN_PROFILE=ds_zero2_safe \
+TRAIN_CUDA_VISIBLE_DEVICES=0,1,2,3 \
+TRAIN_LORA=1 \
+bash scripts/exps/run_one_exp.sh exps/cold/baselines/exp_zero_shot_d55cfa6534
