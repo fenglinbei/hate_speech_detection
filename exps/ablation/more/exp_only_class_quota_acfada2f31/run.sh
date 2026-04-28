0991fc3cdc0341bf4d5b722c16035b39032a4578
fenglinbei <1349410043@qq.com>
2026-04-29 02:24:41 +0800

    new exp

---
+++ exps/ablation/more/exp_only_class_quota_acfada2f31/run.sh
@@ -0,0 +1,6 @@
+MODE=full \
+TRAIN_BACKEND=deepspeed \
+TRAIN_PROFILE=ds_zero2_bs8 \
+TRAIN_CUDA_VISIBLE_DEVICES=0,1,2,3 \
+TRAIN_LORA=1 \
+bash scripts/exps/run_one_exp.sh exps/ablation/more/exp_only_class_quota_acfada2f31
